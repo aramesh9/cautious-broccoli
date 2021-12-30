83a49bb1fdbe4fd45136bbeb266b143bdfdce368
aramesh9 <80480643+aramesh9@users.noreply.github.com>
2021-12-30 11:22:13 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function gotPoses(results){
+    if (results.length>0){
+        console.log(results);
+    }
+}
+function modelLoaded(){
+    console.log('poseNet IS initiated');
+}
+function draw(){
+    background('#30D5C8');
+}
